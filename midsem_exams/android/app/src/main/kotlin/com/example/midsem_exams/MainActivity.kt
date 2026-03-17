2de76bf5407af42b5a227c47839d45d82fc9049d
Bright Oppong Adu-Gyamfi <181453308+Brixco-web@users.noreply.github.com>
2026-03-17 09:40:06 +0000

    feat: phase 1 - project setup and data models

---
+++ midsem_exams/android/app/src/main/kotlin/com/example/midsem_exams/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.midsem_exams
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ midsem_exams/lib/models/student.dart
@@ -0,0 +1,16 @@
+class Student {
+  final String name;
+
+  final String studentId;
+
+  final String programme;
+
+  final int level;
+
+  const Student({
+    required this.name,
+    required this.studentId,
+    required this.programme,
+    required this.level,
+  });
+}
